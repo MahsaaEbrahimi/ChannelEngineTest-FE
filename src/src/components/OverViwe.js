aafbc7653004bd475e91aabec29bba3e4aabcdc8
MahsaaEbrahimi <ebrahimimahsa510@gmail.com>
2024-05-15 18:53:10 +0200

    update pagination

--- src/src/components/OverViwe.js
+++ src/src/components/OverViwe.js
@@ -9,31 +9,37 @@ export default function OverView(){
 
     const apikey ='541b989ef78ccb1bad630ea5b85c6ebff9ca3322';
     const[Loading,setLoading]=useState(false)
-    const[orderNumber,setTotalCount]=useState(0)
+    const[orderTotalCount,setTotalCount]=useState(0)
     const[allOrders,setAllOrders]=useState([])
     const[currentPage,setCurrentPage]=useState(1)
     const[orderPerPage,setOrderPerPage]=useState(10)
     const[status,setStatus]=useState("")
+    const[page,setPage]=useState(1)
+    const[apiItemsPerPage,setApiItemsPerPage]=useState()
 
   
     useEffect(()=>{
         setLoading(true)
-        axios.get(`https://api-dev.channelengine.net/api/v2/orders?apikey=${apikey}`)
+        axios.get(`https://api-dev.channelengine.net/api/v2/orders?page=${page}&apikey=${apikey}`)
         .then(data=>{
             setTotalCount(data?.data?.TotalCount)
             setAllOrders(data?.data?.Content)
+            setApiItemsPerPage(data?.data?.ItemsPerPage)
             setLoading(false)
+           
         })
         .catch((error)=>{
             console.log(error.message)
             setLoading(false)
         })
-    },[])
+    },[page])
+  
+    
 
     function aplayFilter(status){
         if(status==="ALL"){
             setLoading(true)
-            axios.get(`https://api-dev.channelengine.net/api/v2/orders?apikey=${apikey}`)
+            axios.get(`https://api-dev.channelengine.net/api/v2/orders?page=${page}&apikey=${apikey}`)
             .then(data=>{
                 setTotalCount(data?.data?.TotalCount)
                 setAllOrders(data?.data?.Content)
@@ -46,7 +52,7 @@ export default function OverView(){
 
         }else{
             setLoading(true)
-            axios.get(`https://api-dev.channelengine.net/api/v2/orders?statuses=${status}&apikey=${apikey}`)
+            axios.get(`https://api-dev.channelengine.net/api/v2/orders?statuses=${status}&page=${page}&apikey=${apikey}`)
             .then(data=>{
                 setTotalCount(data?.data?.TotalCount)
                 setAllOrders(data?.data?.Content)
@@ -56,6 +62,7 @@ export default function OverView(){
             .catch((error)=>{
                 console.log(error.message)
                 setLoading(false)
+                
             })
         }
     }
@@ -65,15 +72,30 @@ export default function OverView(){
     const indexOfFirstOrder= indexOfLastOrder-orderPerPage
     const currentOrders=allOrders.slice(indexOfFirstOrder,indexOfLastOrder)
 
-    const paginate =(number) => setCurrentPage(number)
+    function paginate(number) {
+        setCurrentPage(number)
+        changePageNumber(number)
+    }
+
+    function changePageNumber(n){
+        let pageN = Math.round((n*orderPerPage)/apiItemsPerPage)
+        if(page !== pageN){
+            if(pageN==0){
+                setPage(1)
+            }else{
+                setPage(pageN)
+            }
+        }
+        
+    }
 
     function reducePageNumber(){
         setCurrentPage(privVal=>privVal-1)
     }
 
     function increasePageNumber(){
-        if(currentPage===Math.ceil(orderNumber/orderPerPage)){
-            setCurrentPage(Math.ceil(orderNumber/orderPerPage))
+        if(currentPage===Math.ceil(orderTotalCount/orderPerPage)){
+            setCurrentPage(Math.ceil(orderTotalCount/orderPerPage))
         }else{
             setCurrentPage(privVal=>privVal+1)
         }
@@ -99,7 +121,7 @@ export default function OverView(){
                 
                 { !Loading &&
                     <Pagination
-                        total={orderNumber}
+                        total={orderTotalCount}
                         orderPerPage={orderPerPage}
                         paginate={paginate}
                         reducePageNumber={reducePageNumber}
